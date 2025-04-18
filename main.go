c2a2fc843f5c72aaa9f234ae8799472d7285f7cc
Josh LeBlanc <joshcapitals07@gmail.com>
2025-04-18 18:20:56 +0200

    run method implemented

--- main.go
+++ main.go
@@ -32,6 +32,38 @@ func main() {
     }
 }
 
+func (a *Agent) Run(ctx context.Context) error {
+	conversation := []anthropic.MessageParam{}
+
+	fmt.Println("Chat with Claude (use 'ctrl-c' to quit)")
+
+	for {
+		fmt.Print("\u001b[94mYou\u001b[0m: ")
+		userInput, ok := a.getUserMessage()
+		if !ok {
+			break
+		}
+
+		userMessage := anthropic.NewUserMessage(anthropic.NewTextBlock(userInput))
+		conversation = append(conversation, userMessage)
+
+		message, err := a.runInference(ctx, conversation)
+		if err != nil {
+			return err
+		}
+		conversation = append(conversation, message.ToParam())
+
+		for _, content := range message.Content {
+			switch content.Type {
+			case "text":
+				fmt.Printf("\u001b[93mClaude\u001b[0m: %s\n", content.Text)
+			}
+		}
+	}
+
+	return nil
+}
+
 func NewAgent(client *anthropic.Client, getUserMessage func() (string, bool)) {
     return &Agent{
 	client: client,
